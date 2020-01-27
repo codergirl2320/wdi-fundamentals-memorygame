47ddab80f6f157b4e0f69bdf770bf3357da5f2e6
Tania Diane Dodson <taniad0916@MacBook.local>
2020-01-27 02:44:08 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,15 +1,18 @@
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push('cardOne');
-let cardTwo = cards[2];
-cardsInPlay.push('cardTwo');
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+function checkForMatch() {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
 	} else {
-		alert("Sorry, try again.");
+		console.log("Sorry, try again.");
 	}
 }
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId])
+		cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+checkForMatch();
+}
+}
+flipCard(0);
+flipCard(2);
